b7623fb35f73e9a885b20cde817abee3340a9111
Aditya Singh <aditya.singh@zluri.com>
2025-05-26 11:45:52 +0530

    replace BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import ChatRoom from "./modules/ChatRoom";
 import ChatApplication from "./modules/ChatApplication";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/home" element={<ChatApplication />} />
           <Route path="/room/:roomId" element={<ChatRoom />} />
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
